2bb9da690391bcae57e644f6bc24e9cf77f05b56
LEO001xD <141621669+LEO001xD@users.noreply.github.com>
2024-01-17 23:34:20 +0700

    Update sketch.ino
    poori gay

--- sketch.ino
+++ sketch.ino
@@ -9,6 +9,10 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
+#include <DHT.h>
+#define DHTPIN 15   // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT sensor type
+DHT dht(DHTPIN, DHTTYPE);
 #define SOI_PIN 4
 
 #define SSID "Wokwi-GUEST"               
@@ -16,7 +20,7 @@
 #define LINE_TOKEN "......................"      
 LiquidCrystal lcd(12, 13, 16, 17, 18, 19);
 HCSR04 hc(23, 22); //tric,echo
-void setup() {
+void lcd() {
   lcd.begin(16, 2);
   Serial.begin(115200);
   Blynk.begin(BLYNK_AUTH_TOKEN, SSID, PASSWORD);
@@ -34,32 +38,41 @@ void setup() {
   //LINE.notifyPicture("uto kawaii","https://cdn.discordapp.com/attachments/1138409352777175150/1188863197151776828/GBOOGKHawAALsGz.jfif?ex=659c1222&is=65899d22&hm=4fff9989b9c2c766ddf79ba549247abfa939ae32417bf16259d9f0b76512031c&");
 }
 
-void soi_moisture() {//lcd show soi mois,temp
+void lcd() {//lcd show soi mois,temp
 
   lcd.setCursor(0, 0);
   lcd.print("Moisture : "); 
-  lcd.print(moisture);
+  lcd.print(misture);o
   lcd.print("        "); 
   delay(100);
   lcd.setCursor(0, 1);
   lcd.print("Temperature : "); // dht ยังไม่เขียน
-  lcd.print(temp);// dht ยังไม่เขียน
+  lcd.print(temperature);// dht ยังไม่เขียน
   lcd.print("        "); 
   delay(100);
 
 }
 void DHT() { //อุณหภูมิในอากาศ เขียนไม่เป็น
+  //dht
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  char mix_temp_humi[50];
+  sprintf(mix_temp_humi, "Temperature %d \n Humidity %d", temperature ,humidity);
+  Blynk.virtualWrite(V2, mix_temp_humi);
 
-  
 }
-void soi_moisture() {//ความขื้นในดิน
-
+void soi_moisture_and_rod_nam() {//ความขื้นในดิน
   int moisture = analogRead(SOI_PIN); // read the analog value from sensor
   Serial.print("Moisture in soi value: ");
   Serial.println(moisture);
   delay(500);
-}
-
+  if(moisture<=80){
+    digitalWrite(Relay1,HIGH);
+    } 
+  else{
+    digitalWrite(Relay1,LOW);
+    } 
+  }
 void water() {//ดูว่าน้ำจะหมดไหม
   int distance = hc.dist();
   if (distance <= 20){
@@ -71,8 +84,9 @@ void water() {//ดูว่าน้ำจะหมดไหม
   }
 }
 void loop() {
+  lcd();
   DHT();
-  soi_moisture();
+  soi_moisture_and_rod_nam();
   water();
   delay(100);
   Blynk.run();
